6fbde425653eb9b897fa242b5c56f9c5db0dec83
jordannaegle <jordanchasenaegle@gmail.com>
2023-04-25 18:40:09 -0600

    map

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,5 +1,9 @@
 <template>
    <h1>Map</h1>
+   <div class="map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3043.866046654194!2d-111.71608200604764!3d40.27872573843891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x874d9b1eeeb9f5c5%3A0x63523d8413e98aa1!2sUtah%20Valley%20University!5e0!3m2!1sen!2sus!4v1682398195958!5m2!1sen!2sus" 
+    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+   </div>
 </template>
 
 <script>
@@ -26,5 +30,8 @@ export default {
 </script>
 
 <style scoped>
-
+  .map {
+    height: 100%;
+    width: 100%;
+  }
 </style>
